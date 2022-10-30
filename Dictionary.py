44ee5fdaf77558557176e985df9c49cfd07b0bc1
EstalinP <epenacanario9633@conestogac.on.ca>
2022-10-30 16:40:06 -0400

    Final 1.1

--- Dictionary.py
+++
@@ -1,7 +0,0 @@
-n=int(input("Input a number "))
-d = dict()
-
-for x in range(1,11):
-    d[x]=x**3
-
-print(d) 
--- tuples.py
+++
@@ -1,4 +0,0 @@
-fruit = ("apple", "banana", "cherry", "lemon", "watermelon", "cherry")
-print(fruit[1]) # Prints the second item in the tuple
-print(fruit[-1]) # Negative indexing means start from the end; prints out the last item
-print(fruit[2:5])
